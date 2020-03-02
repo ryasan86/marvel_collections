3a00906d9e5fe756874f565a6c69c1f32bdbc5ec
Ryan Santos <ryan@Ryan-Santoss-MacBook-Pro.local>
2020-03-02 08:43:22 -0800

    layout wrapper on root

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from 'react'
+import GlobalStyles from './src/styles/GlobalStyles'
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => {
+  return (
+    <>
+      <GlobalStyles />
+      {element}
+    </>
+  )
+}
--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 import Header from './Header'
 import Footer from './Footer'
 import Main from '../styles/MainStyles'
-import GlobalStyles from '../styles/GlobalStyles'
 
 const StyledLayout = styled.div`
   height: 100%;
@@ -15,7 +14,6 @@ const StyledLayout = styled.div`
 
 const Layout = ({ children }) => (
   <StyledLayout>
-    <GlobalStyles />
     <Header />
     <Main>{children}</Main>
     <Footer />
