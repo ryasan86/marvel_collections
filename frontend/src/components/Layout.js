be8b64371d6669be43bf9db446da2c8838f9edee
Ryan Santos <ryan@Ryan-Santoss-MacBook-Pro.local>
2020-03-02 08:13:25 -0800

    using createGlobalStyles

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import Header from './Header'
 import Footer from './Footer'
 import Main from '../styles/MainStyles'
-import '../styles/global.css'
+import GlobalStyles from '../styles/GlobalStyles'
 
 const StyledLayout = styled.div`
   height: 100%;
@@ -15,6 +15,7 @@ const StyledLayout = styled.div`
 
 const Layout = ({ children }) => (
   <StyledLayout>
+    <GlobalStyles />
     <Header />
     <Main>{children}</Main>
     <Footer />
