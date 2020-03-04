1190dffc615ae18b9513671c89a80a24ab48b899
Ryan Santos <ryan@ryansantossmbp.home>
2020-03-03 21:06:59 -0800

    allow dev and prod cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,13 +12,20 @@ server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 server.use(cookieParser())
 
+const { DEV_FRONTEND_URL, PROD_FRONTEND_URL } = process.env
+
 const options = {
   cors: {
     credentials: true,
-    origin: process.env.FRONTEND_URL
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? DEV_FRONTEND_URL
+        : PROD_FRONTEND_URL
   },
   endpoint: '/',
   playground: '/playground'
 }
 
-server.start(options, deets => console.log('Server is running on port: ', deets.port))
+server.start(options, deets =>
+  console.log('Server is running on port: ', deets.port)
+)
